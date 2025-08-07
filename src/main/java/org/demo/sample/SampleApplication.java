6f27589e5e03948a67d984d9b7f8f0c829d16dba
GabinL21 <gabin.laigle@sonarsource.com>
2025-08-07 09:36:34 +0200

    Add perpexlity hardcoded key

--- src/main/java/org/demo/sample/SampleApplication.java
+++ src/main/java/org/demo/sample/SampleApplication.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 public class SampleApplication {
 
   public static void main(String[] args) {
+    String perpexlityKey = "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";
     SpringApplication.run(SampleApplication.class, args);
   }
 
